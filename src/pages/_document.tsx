8ce948d8ba70ab148c74027a9b57bef7b235c55d
Tom Lamb <lambtsa.tom@gmail.com>
2022-12-20 09:05:37 +0000

    Update html lang

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,7 +41,7 @@ export default class MyDocument extends Document {
 
   public render(): JSX.Element {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
