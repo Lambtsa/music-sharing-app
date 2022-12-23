e45fac0b20e812b88443a1caf4efeb89cf55f10e
Tom Lamb <lambtsa.tom@gmail.com>
2022-12-23 18:23:33 +0000

    Update prod/devs urls

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,6 +1,6 @@
 export default {
   DEV: "http://localhost:3000",
-  PROD: "https://www.charlies-closet.com",
+  PROD: "https://audiolinx.xyz",
   FACEBOOK: "https://www.facebook.com/charlies-closet",
   INSTAGRAM: "https://www.instagram.com/charlies-closet",
   TWITTER: "https://www.charlies-closet.com",
