529b99c0d7be75298af666c123f2e9ed25f24919
Tom Lamb <lambtsa.tom@gmail.com>
2023-01-23 11:24:42 +0100

    Update twitter metadata

--- src/components/MetaData/MetaData.tsx
+++ src/components/MetaData/MetaData.tsx
@@ -24,6 +24,11 @@ export const MetaData = ({
           <meta name="googlebot" content="index, follow" />
         </>
       )}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={url} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content="/og-image.png" />
     </Head>
   );
 };
